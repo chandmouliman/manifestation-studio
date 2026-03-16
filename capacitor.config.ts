b7ddaab0c9e479bc238040111259f0d75d13c8f2
Chandra Mouli <chandramouli19@gmail.com>
2026-03-16 17:19:36 +0530

    feat: integrate Capacitor for Android deployment

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.chandmouliman.manifestation',
+  appName: 'Manifestation Studio',
+  webDir: 'dist'
+};
+
+export default config;
